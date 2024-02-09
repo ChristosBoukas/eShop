c9c771622456842d27d12dc59ded11179a7db6d6
Christos Boukas <Christosboukast@gmail.com>
2024-02-09 16:18:42 +0100

    Started implementing CategoryHttpClient

---
+++ eShop.UI.Http/Clients/CategoryHttpClient.cs
@@ -0,0 +1,14 @@
+﻿namespace eShop.UI.Http.Clients;
+
+public class CategoryHttpClient
+{
+    private readonly HttpClient _httpClient;
+    string _baseAdress = "https://localhost:5000/api/";
+
+    public CategoryHttpClient(HttpClient httpclient)
+    {
+        _httpClient = httpclient;
+        _httpClient.BaseAddress = new Uri($"{_baseAdress}categorys");
+    }
+
+}
--- eShop.UI/Services/UIService.cs
+++ eShop.UI/Services/UIService.cs
@@ -2,4 +2,5 @@
 
 public class UIService(CategoryHttpClient categoryHttp)
 {
+
 }
