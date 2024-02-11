c092533235c64ff941aba452807bc9815dfda8a0
Christos Boukas <Christosboukast@gmail.com>
2024-02-11 14:16:07 +0100

    added more stuff to aside component and UI Service

--- eShop.UI/Services/UIService.cs
+++ eShop.UI/Services/UIService.cs
@@ -2,5 +2,22 @@
 
 public class UIService(CategoryHttpClient categoryHttp)
 {
+    List<CategoryGetDTO> Categories { get; set; } = [];
+
+    public List<LinkGroup> CategoryLinkGroups { get; private set; } =
+    [
+        new LinkGroup
+        {
+            Name = "Categories"
+            ,
+            LinkOptions = new(){
+                new LinkOption { Id = 1, Name = "Women", IsSelected = true },
+                new LinkOption { Id = 2, Name = "Men", IsSelected = false },
+                new LinkOption { Id = 3, Name = "Children", IsSelected = false }
+            }
+        }
+    ];
+
+    public int CurrentCategoryId { get; set; }
 
 }
